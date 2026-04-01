3b9db48812cc51b2451275b3ae25bd4a2f388b4d
christian.conti <christian.conti@polito.it>
2026-04-01 19:24:53 +0200

    Fix wrongly updated files

--- hw/asic/generic_example/rtl/prim_mytech_clk.sv
+++ hw/asic/generic_example/rtl/prim_mytech_clk.sv
@@ -3,10 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 
 module mytech_clk_gating (
-    input  logic clk_i,
-    input  logic en_i,
-    input  logic test_en_i,
-    output logic clk_o
+   input  logic clk_i,
+   input  logic en_i,
+   input  logic test_en_i,
+   output logic clk_o
 );
 
 
@@ -18,8 +18,8 @@ module mytech_clk_gating (
 endmodule
 
 module mytech_clk_inverter (
-    input  logic clk_i,
-    output logic clk_o
+  input  logic clk_i,
+  output logic clk_o
 );
 
   /*
@@ -30,10 +30,10 @@ endmodule
 
 
 module mytech_clk_mux2 (
-    input  logic clk0_i,
-    input  logic clk1_i,
-    input  logic clk_sel_i,
-    output logic clk_o
+  input  logic clk0_i,
+  input  logic clk1_i,
+  input  logic clk_sel_i,
+  output logic clk_o
 );
 
   /*
@@ -42,38 +42,42 @@ module mytech_clk_mux2 (
 
 endmodule
 
-module cluster_clock_inverter (
-    input  logic clk_i,
-    output logic clk_o
+module cluster_clock_inverter(
+  input  logic clk_i,
+  output logic clk_o
 );
 
-  mytech_clk_inverter clk_inv_i (.*);
+  mytech_clk_inverter clk_inv_i (
+    .*
+  );
 
 endmodule
 
 module pulp_clock_mux2 (
-    input  logic clk0_i,
-    input  logic clk1_i,
-    input  logic clk_sel_i,
-    output logic clk_o
+  input  logic clk0_i,
+  input  logic clk1_i,
+  input  logic clk_sel_i,
+  output logic clk_o
 );
 
-  mytech_clk_mux2 clk_mux2_i (.*);
+  mytech_clk_mux2 clk_mux2_i (
+    .*
+  );
 
 endmodule
 
 module cv32e40p_clock_gate (
-    input  logic clk_i,
-    input  logic en_i,
-    input  logic scan_cg_en_i,
-    output logic clk_o
+   input  logic clk_i,
+   input  logic en_i,
+   input  logic scan_cg_en_i,
+   output logic clk_o
 );
 
   mytech_clk_gating clk_gate_i (
-      .clk_i,
-      .en_i,
-      .test_en_i(scan_cg_en_i),
-      .clk_o
+    .clk_i,
+    .en_i,
+    .test_en_i(scan_cg_en_i),
+    .clk_o
   );
 
 endmodule
